ff3f3b92c72ae534b74f43cbdbabec260a6c6cb8
Jayesh <dhotrejayesh@gmail.com>
2026-02-07 19:38:40 +0530

    first commit

---
+++ parent/step2-openai/src/test/java/com/jayesh/javaspringai/Step2OpenaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayesh.javaspringai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Step2OpenaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
